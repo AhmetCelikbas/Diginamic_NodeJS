8541d9209f49375390286ac41a49fd23a51a8527
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-09-29 16:16:41 +0200

    Upgrade and refactor pub-cli code to ES6+

--- pub-cli/bin/index.js
+++ pub-cli/bin/index.js
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
-var program = require('commander');
-var main = require('../src/main');
+const program = require('commander');
+const main = require('../src/main');
 
 program
-    .version('1.0.0')
-    .option('-l, --list', 'Get the pubs list')
-    .option('-o, --opened', 'Get the pubs list')
-    .parse(process.argv);
-
+  .version('1.0.0')
+  .option('-l, --list', 'Get the pubs list')
+  .option('-o, --opened', 'Get the pubs list')
+  .parse(process.argv);
 
 
 if (program.list) {
-    console.log(main.getListPub());
+  console.log(main.getListPub());
 }
 
 if (program.opened) {
-    console.log(main.getListPubsOuvertAujourdHui());
+  console.log(main.getListPubsOuvertAujourdHui());
 }
