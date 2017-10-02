70f5c27ed9ba5af861f47f240bf1ccd3d3e90e47
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-10-02 09:25:41 +0200

    TP3.2 terminé

---
+++ tp3-2/index.js
@@ -0,0 +1,5 @@
+const main = require('./src/main');
+
+main();
+
+module.exports = main;
---
+++ tp3-2/src/services/data.service.js
@@ -0,0 +1,97 @@
+module.exports = [
+  {
+    "name": "Arawak",
+    "owner": {
+      "firstName": "Nicolas",
+      "lastName": "Hodicq",
+      "mail": "nhodicq@bewizyu.com"
+    },
+    "openDays": [
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ],
+    "openHours": {
+      "start": 15,
+      "end": 16
+    },
+    "beers": [
+      {
+        "type": "Blonde",
+        "name": "Triple Karmeliet"
+      }
+    ]
+  },
+  {
+    "name": "YoloPub",
+    "owner": {
+      "firstName": "Ahmet",
+      "lastName": "Celikbas",
+      "mail": "celikbas.ahmet@gmail.com"
+    },
+    "openDays": [
+      "Friday",
+      "Sunday"
+    ],
+    "openHours": {
+      "start": 15,
+      "end": 0
+    },
+    "beers": [
+      {
+        "type": "Brune",
+        "name": "Tabernakle"
+      }
+    ]
+  },
+  {
+    "name": "ChevrePub",
+    "owner": {
+      "firstName": "Ahmet",
+      "lastName": "Celikbas",
+      "mail": "celikbas.ahmet@gmail.com"
+    },
+    "openDays": [
+      "Sunday",
+      "Friday",
+      "Wednesday",
+      "Thursday"
+    ],
+    "openHours": {
+      "start": 10,
+      "end": 22
+    },
+    "beers": [
+      {
+        "type": "Brune",
+        "name": "Tabernakle"
+      }
+    ]
+  },
+  {
+    "name": "BrebiePub",
+    "owner": {
+      "firstName": "Ahmet",
+      "lastName": "Celikbas",
+      "mail": "celikbas.ahmet@gmail.com"
+    },
+    "openDays": [
+      "Sunday",
+      "Friday",
+      "Wednesday",
+      "Thursday"
+    ],
+    "openHours": {
+      "start": 13,
+      "end": 16
+    },
+    "beers": [
+      {
+        "type": "Brune",
+        "name": "Tabernakle"
+      }
+    ]
+  }
+];
